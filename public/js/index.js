7dc41135ffa4e10a73664938461b02176bad2647
Fernando <fandradecamacho@gmail.com>
2017-08-28 00:02:11 -0300

    Scroll to bottom if users is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@
 
 var socket = io()
 
+function scrollToBotton() {
+    // Selectors
+    var messages = jQuery('#messages')
+    var newMessage = messages.children('li:last-child')
+    // Heights
+    var clientHeight = messages.prop('clientHeight')
+    var scrollTop = messages.prop('scrollTop')
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight()
+    var lastMessageHeight = newMessage.prev().innerHeight()
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight)
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server')
 })
@@ -20,6 +36,7 @@ socket.on('newMessage', function (message) {
     })
 
     jQuery('#messages').append(html)
+    scrollToBotton()
 })
 
 socket.on('newLocationMessage', function (message) {
@@ -32,6 +49,7 @@ socket.on('newLocationMessage', function (message) {
     })
     
     jQuery('#messages').append(html)
+    scrollToBotton()
 })
 
 jQuery('#message-form').on('submit', function (e ) {
